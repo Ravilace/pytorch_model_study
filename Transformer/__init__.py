b4074d30bd78e2a9e54a5d65158c21500cf6380e
Ravilace <wujinjun65@gmail.com>
2025-03-26 10:58:06 +0800

    :sparkles: 完成clone

--- Transformer/__init__.py
+++ Transformer/__init__.py
@@ -3,3 +3,10 @@
 # @Author  : Ravilace
 # @File    : __init__.py.py
 # @Software: PyCharm
+import copy
+
+import torch.nn as nn
+
+def clones(module, N):
+    """产生N个相同的module"""
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
