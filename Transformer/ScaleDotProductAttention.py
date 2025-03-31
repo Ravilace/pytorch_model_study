f4078825cbcaca2c55254f32cb9dc3ce45495e26
Ravilace <wujinjun65@gmail.com>
2025-03-31 15:48:29 +0800

    :tada: ScaleDotProductAttention初始化

---
+++ Transformer/ScaleDotProductAttention.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/3/31 15:41
+# @Author  : Ravilace
+# @File    : ScaleDotProductAttention.py
+# @Software: PyCharm
+from torch import nn
+
+
+class ScaleDotProductAttention(nn.Module):
+    pass
+
