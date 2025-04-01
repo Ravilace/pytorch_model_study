ec1f45a729dc34dd045a9d6af961762a85251187
Ravilace <wujinjun65@gmail.com>
2025-04-01 15:05:57 +0800

    :sparkles: 完成编码器

---
+++ Transformer/Encoder.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/4/1 11:22
+# @Author  : Ravilace
+# @File    : Encoder.py
+# @Software: PyCharm
+from torch import nn
+
+from pytorch_model_study.Transformer.EncoderLayer import EncoderLayer
+from pytorch_model_study.Transformer.PositionalEncoding import PositionalEncoding
+
+
+class Encoder(nn.Module):
+    def __init__(self, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+        self.emb = PositionalEncoding(d_model, max_len, device)
+        self.layers = nn.ModuleList(
+            [EncoderLayer(d_model, ffn_hidden, n_head, drop_prob)
+             for _ in range(n_layers)]
+        )
+
+    def forward(self, x):
+        x = self.emb(x)
+
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
